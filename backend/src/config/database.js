79731b8c9576dfdea9ba7aa75ed8aa1ba4d12b7c
Khushee-coder <khushinmasurkar28@gmail.com>
2026-06-17 21:02:50 +0530

    fix aiven ssl connection

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,   // 🔴 IMPORTANT FIX
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -13,6 +13,7 @@ const pool = mysql.createPool({
     queueLimit: 0,
 
     ssl: {
+        minVersion: 'TLSv1.2',
         rejectUnauthorized: false
     }
 });
